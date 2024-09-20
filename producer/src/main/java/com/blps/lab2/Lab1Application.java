0438716f891c4c11fb0a8a51eeb63feac3480f03
GrigAnd <andrey.grigorev03@gmail.com>
2024-09-20 19:07:44 +0300

    camunda

--- producer/src/main/java/com/blps/lab2/Lab1Application.java
+++ producer/src/main/java/com/blps/lab2/Lab1Application.java
@@ -1,19 +1,21 @@
 package com.blps.lab2;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class Lab1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab1Application.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Lab1Application.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(Lab1Application.class);
+//	}
 }
--- producer/src/main/java/com/blps/lab2/config/security/SecurityConfiguration.java
+++ producer/src/main/java/com/blps/lab2/config/security/SecurityConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -25,6 +27,10 @@ public class SecurityConfiguration {
                         .anyRequest().permitAll())
 
                 .httpBasic(withDefaults())
+                .csrf(CsrfConfigurer::disable) // Disable CSRF protection
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // Create session if required
+                )
 
         ;
         return http.build();
